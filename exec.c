2b7b3e597fc0108eafee4de09d5675c0317ed3b4
Martin Kihungi <mwangi4786@gmail.com>
2023-07-26 04:13:56 -0700

    Shell

--- exec.c
+++ exec.c
@@ -1,26 +1,47 @@
 #include "main.h"
-
-
-
 /**
- * exec - perform command execution.
- * @argv: pointer to command line variables vector.
- *
- * Return: NULL
+ * execute - execute a command with its entire path variables.
+ * @data: a pointer to the program's data
+ * Return: If sucess returns zero, otherwise, return -1.
  */
-
-
-void exec(char **argv)
+int execute(data_of_program *data)
 {
-	char *argv_zero = NULL, *command = NULL;
+	int retval = 0, status;
+	pid_t pidd;
 
-	if (argv)
-	{
-		argv_zero = argv[0];
-		command = get_location(argv_zero);
-		if (execve(command, argv, NULL) == -1)
-		{
-			perror("./shell");
+	/* check for program in built ins */
+	retval = builtins_list(data);
+	if (retval != -1)/* if program was found in built ins */
+		return (retval);
+
+	/* check for program file system */
+	retval = find_program(data);
+	if (retval)
+	{/* if program not found */
+		return (retval);
+	}
+	else
+	{/* if program was found */
+		pidd = fork(); /* create a child process */
+		if (pidd == -1)
+		{ /* if the fork call failed */
+			perror(data->command_name);
+			exit(EXIT_FAILURE);
+		}
+		if (pidd == 0)
+		{/* I am the child process, I execute the program*/
+			retval = execve(data->tokens[0], data->tokens, data->env);
+			if (retval == -1) /* if error when execve*/
+				perror(data->command_name), exit(EXIT_FAILURE);
+		}
+		else
+		{/* I am the father, I wait and check the exit status of the child */
+			wait(&status);
+			if (WIFEXITED(status))
+				errno = WEXITSTATUS(status);
+			else if (WIFSIGNALED(status))
+				errno = 128 + WTERMSIG(status);
 		}
 	}
+	return (0);
 }
--- get_location.c
+++ get_location.c
@@ -1,52 +1,123 @@
 #include "main.h"
 
+int check_file(char *full_path);
+
 /**
- * get_location - populates the command's path.
- * @argv_zero: pointer to env. variables list.
- *
- * Return: NULL
-*/
+ * find_program - find a program in path
+ * @data: a pointer to the program's data
+ * Return: 0 if success, errcode otherwise
+ */
 
-
-char *get_location(char *argv_zero)
+int find_program(data_of_program *data)
 {
-	char *path, *path_copy, *path_token, *file_path;
-	int argv_zero_length, dir_len;
-	struct stat buffer;
+	int i = 0, ret_code = 0;
+	char **directories;
 
-	path = getenv("PATH");
+	if (!data->command_name)
+		return (2);
 
-	if (path)
+	/**if is a full_path or an executable in the same path */
+	if (data->command_name[0] == '/' || data->command_name[0] == '.')
+		return (check_file(data->command_name));
+
+	free(data->tokens[0]);
+	data->tokens[0] = str_concat(str_duplicate("/"), data->command_name);
+	if (!data->tokens[0])
+		return (2);
+
+	directories = tokenize_path(data);/* search in the PATH */
+
+	if (!directories || !directories[0])
 	{
-		path_copy = strdup(path);
-		argv_zero_length = strlen(argv_zero);
-		path_token = strtok(path_copy, ":");
-
-		while (path_token != NULL)
-		{
-			dir_len = strlen(path_token);
-			file_path = malloc(argv_zero_length + dir_len + 2);
-			strcpy(file_path, path_token);
-			strcat(file_path, "/");
-			strcat(file_path, argv_zero);
-			strcat(file_path, "\0");
-
-			if (stat(file_path, &buffer) == 0)
-			{
-				free(path_copy);
-				return (file_path);
-			}
-			else
-			{
-				free(file_path);
-				path_token = strtok(NULL, ":");
-			}
+		errno = 127;
+		return (127);
+	}
+	for (i = 0; directories[i]; i++)
+	{/* appends the function_name to path */
+		directories[i] = str_concat(directories[i], data->tokens[0]);
+		ret_code = check_file(directories[i]);
+		if (ret_code == 0 || ret_code == 126)
+		{/* the file was found, is not a directory and has execute permisions*/
+			errno = 0;
+			free(data->tokens[0]);
+			data->tokens[0] = str_duplicate(directories[i]);
+			free_array_of_pointers(directories);
+			return (ret_code);
 		}
-		if (stat(argv_zero, &buffer) == 0)
-		{
-			return (argv_zero);
-		}
+	}
+	free(data->tokens[0]);
+	data->tokens[0] = NULL;
+	free_array_of_pointers(directories);
+	return (ret_code);
+}
+
+/**
+ * tokenize_path - tokenize the path in directories
+ * @data: a pointer to the program's data
+ * Return: array of path directories
+ */
+
+char **tokenize_path(data_of_program *data)
+{
+	int i = 0;
+	int counter_directories = 2;
+	char **tokens = NULL;
+	char *PATH;
+
+	/* get the PATH value*/
+	PATH = env_get_key("PATH", data);
+	if ((PATH == NULL) || PATH[0] == '\0')
+	{/*path not found*/
 		return (NULL);
 	}
-	return (NULL);
+
+	PATH = str_duplicate(PATH);
+
+	/* find the number of directories in the PATH */
+	for (i = 0; PATH[i]; i++)
+	{
+		if (PATH[i] == ':')
+			counter_directories++;
+	}
+
+	/* reserve space for the array of pointers */
+	tokens = malloc(sizeof(char *) * counter_directories);
+
+	/*tokenize and duplicate each token of path*/
+	i = 0;
+	tokens[i] = str_duplicate(_strtok(PATH, ":"));
+	while (tokens[i++])
+	{
+		tokens[i] = str_duplicate(_strtok(NULL, ":"));
+	}
+
+	free(PATH);
+	PATH = NULL;
+	return (tokens);
+
+}
+
+/**
+ * check_file - checks if exists a file, if it is not a dairectory and
+ * if it has excecution permisions for permisions.
+ * @full_path: pointer to the full file name
+ * Return: 0 on success, or error code if it exists.
+ */
+
+int check_file(char *full_path)
+{
+	struct stat sb;
+
+	if (stat(full_path, &sb) != -1)
+	{
+		if (S_ISDIR(sb.st_mode) ||  access(full_path, X_OK))
+		{
+			errno = 126;
+			return (126);
+		}
+		return (0);
+	}
+	/*if not exist the file*/
+	errno = 127;
+	return (127);
 }
